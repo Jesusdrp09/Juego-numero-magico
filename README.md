006d84b846d1993f1cfb314a97712ac68b2506e8
jesusdrp09 <jesusdrp09@gmail.com>
2021-10-14 10:50:00 -0500

    commit encima del primer commit remoto

--- README.md
+++
@@ -1,2 +0,0 @@
-# Juego-numero-magico
-El numero magico trata de intentar adivinar un numero de 1 al 1000
---
+++ js/codigo.js
@@ -0,0 +1,82 @@
+let validar = document.getElementById("validar");
+let reiniciar = document.getElementById("reiniciar");
+let iniciar = document.getElementById("iniciar");
+let numero = document.getElementById("numero");
+let niveles = document.getElementById("niveles");
+let parrafo = document.getElementById("parrafo");
+let tiempoP = document.getElementById("tiempo");
+let numeroAleatorio = parseInt(Math.random()*1000+1);
+let gana = false;
+var tiempo = 60000;
+
+console.log(numeroAleatorio);
+
+iniciar.onclick = ()=>{
+    gana = false;
+    tiempoP.hidden = false;
+    habilitar();
+    parrafo.innerText = "Ingresa un numero";
+    juego();
+    if(niveles.value == 1){
+        tiempo = 60000;
+    }else if(niveles.value == 2){
+        tiempo = 40000;
+    }else if(niveles.value == 3){
+        tiempo = 30000;
+    }
+    window.setInterval(function(){
+        if(tiempo == 0){
+            if(!gana){
+                parrafo.innerText = `Perdiste, el número mágico es: ${numeroAleatorio}`;
+                validar.disabled = true;
+                numero.disabled = true;
+                iniciar.disabled = true;
+                niveles.disabled = false;
+            }
+        }else{
+            tiempoP.innerHTML = tiempo/1000;
+            tiempo -= 1000;
+        }
+      },1000);
+}
+
+const habilitar = () =>{
+    validar.disabled = false;
+    numero.disabled = false;
+    iniciar.disabled = true;
+    niveles.disabled = true;
+}
+
+function juego() {
+    validar.onclick = ()=>{
+        esIgual(gana);
+    }
+    
+    numero.onkeypress = (e)=>{
+        if(e.keyCode == 13){
+            esIgual(gana);
+        }
+    }
+    
+    const esIgual = (gana)=>{
+        if(!gana){
+            if(numero.value == numeroAleatorio){
+                parrafo.innerText = "Ganaste";
+                gana = true;
+                validar.disabled = true;
+                numero.disabled = true;
+                iniciar.disabled = true;
+                niveles.disabled = true;
+                tiempoP.hidden = true;
+            }else if(numero.value < numeroAleatorio){
+                parrafo.innerText = `El numero es mayor`;
+            }else if(numero.value > numeroAleatorio){
+                parrafo.innerText = `El numero es menor`;
+            }
+        }
+    }
+    
+    reiniciar.onclick = ()=>{
+        location.reload();
+    }
+}
